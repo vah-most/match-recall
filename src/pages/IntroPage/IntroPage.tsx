3013a776314db931f2ea7d09f8dffe1acf1c55d6
vahabzadeh_m <vahabzadeh_m@office.zharfpouyan.net>
2023-04-23 21:27:23 +0430

    Improved style a bit

--- src/pages/IntroPage/IntroPage.tsx
+++ src/pages/IntroPage/IntroPage.tsx
@@ -69,7 +69,7 @@ const IntroPage = (): ReactElement => {
                 value={username.value}
               />
             ) : (
-              <div className="usernameText">{username.value}!</div>
+              <div className="usernameText">{username.value}</div>
             )}
           </div>
         </div>
